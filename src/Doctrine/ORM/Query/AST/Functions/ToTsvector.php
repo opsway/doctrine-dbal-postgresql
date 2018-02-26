354a334a0aac29d38745da1823900ec0dc2a2e9b
Davi Vidal <davi.vidal@snowmanlabs.com.br>
2018-02-26 16:14:52 -0300

    Supports to_tsquery and to_tsvector config parameter

--- src/Doctrine/ORM/Query/AST/Functions/ToTsvector.php
+++ src/Doctrine/ORM/Query/AST/Functions/ToTsvector.php
@@ -9,6 +9,8 @@ use Doctrine\ORM\Query\SqlWalker;
 
 class ToTsvector extends FunctionNode
 {
+    private $config = null;
+
     private $expr1;
 
     public function parse(Parser $parser)
@@ -16,14 +18,29 @@ class ToTsvector extends FunctionNode
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
         $this->expr1 = $parser->StringExpression(); //SingleValuedPathExpression();
+
+        if ($parser->getLexer()->isNextToken(Lexer::T_COMMA)) {
+            $parser->match(Lexer::T_COMMA);
+            $this->config = $this->expr1;
+            $this->expr1 = $parser->StringExpression();
+        }
+
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker)
     {
-        return sprintf(
-            'to_tsvector(%s)',
-            $this->expr1->dispatch($sqlWalker)
-        );
+        if (null === $this->config) {
+            return sprintf(
+                'to_tsvector(%s)',
+                $this->expr1->dispatch($sqlWalker)
+            );
+        } else {
+            return sprintf(
+                'to_tsvector(%s, %s)',
+                $this->config->dispatch($sqlWalker),
+                $this->expr1->dispatch($sqlWalker)
+            );
+        }
     }
 }
