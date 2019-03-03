63e589bca1ad987c1b4a6c6c915013ef4a6855cd
Ilyar <ilyar.software@gmail.com>
2019-03-03 19:10:10 +0200

    test: add test env with EntityManager

---
+++ tests/EmTestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace OpsWay\Tests;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use PHPUnit\Framework\TestCase;
+
+class EmTestCase extends TestCase
+{
+    /** @var EntityManager */
+    protected $em;
+
+    protected function customStringFunctions() : array
+    {
+        return [];
+    }
+
+    protected function setUp()
+    {
+        $config = Setup::createConfiguration(true);
+        $driverImpl = $config->newDefaultAnnotationDriver([__DIR__]);
+        $config->setMetadataDriverImpl($driverImpl);
+        $this->em = EntityManager::create([
+            'driver' => 'pdo_sqlite',
+            'memory' => true,
+        ], $config);
+
+        foreach ($this->customStringFunctions() as $name => $className) {
+            $this->em->getConfiguration()->addCustomStringFunction($name, $className);
+        }
+    }
+}
+
